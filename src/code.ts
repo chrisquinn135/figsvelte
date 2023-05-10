b26fe32ea6b6de5671132d67efd67c332963478b
chrisquinn135 <85804252+chrisquinn135@users.noreply.github.com>
2023-05-09 18:52:57 -0700

    adding backend

--- src/code.ts
+++ src/code.ts
@@ -1,46 +1,74 @@
 // This plugin will open a modal to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
-
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, {themeColors: true, width: 232, height: 208});
+figma.showUI(__html__, { themeColors: true });
 
+let darkSurface0: BaseStyle;
+let lightSurface1: BaseStyle;
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
+async function getStyles() {
+	// grabbing the test styles
+	darkSurface0 = await figma.importStyleByKeyAsync('abc2d581e122b5a0426eccf256503e0570115976');
+	lightSurface1 = await figma.importStyleByKeyAsync('4baf19645d0857995f045fea92c38e80067f605a');
+}
+
+getStyles();
+
 figma.ui.onmessage = msg => {
-	// One way of distinguishing between different types of messages sent from
-	// your HTML page is to use an object with a "type" property like this.
-	if (msg.type === 'create-shapes') {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
 
-		const nodes: SceneNode[] = [];
+	// Change to Light Theme
+    if (msg.type === 'light') {
+		// const styles = await figma.importStyleByKeyAsync();
+		// console.log(styles)
+		// const styles = figma.getStyleById('S:4baf19645d0857995f045fea92c38e80067f605a,');
+		// console.log(styles)
+       traverse(figma.currentPage,'Light')
+    }
 
-		for (let i = 0; i < msg.count; i++) {
+	// Change to Dark Theme
+    if (msg.type === 'dark') {
+        traverse(figma.currentPage,'Dark')
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    // figma.closePlugin();
+};
 
-			var shape;
+// iterate through the whole page
+function traverse(node, theme) {
+    if ("children" in node) {
 
-			if (msg.shape === 'rectangle') {
-				shape = figma.createRectangle();
-			} else if (msg.shape === 'triangle') {
-				shape = figma.createPolygon();
-			} else {
-				shape = figma.createEllipse();
+		// if the node is an instance, switch it to the opposite theme.
+        if (node.type == "INSTANCE") {
+            if (node.componentProperties.hasOwnProperty('Theme')) {
+                if(node.componentProperties.Theme.value != theme) {
+                    node.setProperties({ Theme: theme })
+                } 
+            }
+        }
+
+		// if it is not an instance, update colors and recursively call its children
+        if (node.type !== "INSTANCE") {
+			// check if the node has a fill
+            if (node.fillStyleId) {
+				if (node.fillStyleId.includes(darkSurface0.id) || darkSurface0.id.includes(node.fillStyleId) && theme == 'Light') {
+
+				}
+				else if (node.fillStyleId.includes(lightSurface1.id) || lightSurface1.id.includes(node.fillStyleId) && theme == 'Dark') {
+					node.fillStyleId = darkSurface0.id
+				}
 			}
 
-			shape.x = i * 150;
-			shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-			figma.currentPage.appendChild(shape);
-			nodes.push(shape);
-		}
+			// iterate through the child nodes
+            for (const child of node.children) {
+                traverse(child, theme)
+            }
+        }
+    }
+}
 
-		figma.currentPage.selection = nodes;
-		figma.viewport.scrollAndZoomIntoView(nodes);
-	}
-
-	// Make sure to close the plugin when you're done. Otherwise the plugin will
-	// keep running, which shows the cancel button at the bottom of the screen.
-	figma.closePlugin();
-};
